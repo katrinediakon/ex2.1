f92546831109b09a6bc83c3f85f86ff3ef2cadbe
Kate <Katrinekatek@gmail.com>
2019-11-19 16:06:49 +0300

    ex2-34

---
+++ local/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
@@ -0,0 +1,6 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+
+$arResult['SPECIALDATE'] = $arResult['ITEMS'][0]['TIMESTAMP_X'];
+$this->__component->SetResultCacheKeys(array("SPECIALDATE" ));
