7afe93512d934ce0dc52c73179777d6c8c0ddfc1
Kate <Katrinekatek@gmail.com>
2019-11-25 16:11:29 +0300

    [ex2-25]

---
+++ local/components/mycomponent/complexcomp.exam-materials/templates/.default/exampage.php
@@ -0,0 +1,4 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<p> Param 1 = <?=$arResult['VARIABLES']['PARAM1']?> </p>
+<p> Param 2 = <?=$arResult['VARIABLES']['PARAM2']?></p>
